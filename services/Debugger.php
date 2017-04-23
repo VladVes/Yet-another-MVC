71a5378c6bfbdf3b80693ab57b5ad6e96d968de7
Vlad Ves <vccs@inbox.ru>
2017-04-23 14:43:48 +0300

    little debagger

---
+++ services/Debugger.php
@@ -0,0 +1,33 @@
+<?php 
+namespace app\services;
+
+class Debugger
+{
+	public static function vd($var)
+	{
+		echo "<pre>";
+		var_dump($var);
+		echo "<pre>";
+	}
+
+	public function showGlobalArrs($arr = 'all')
+	{
+		if($arr != 'all') {
+			self::vd($arrName);
+		}else {
+			self::vd($_REQUEST);
+			self::vd($_SERVER);
+			self::vd($_GET);
+			self::vd($_POST);
+			self::vd($_FILES);
+			self::vd($_COOKIE);
+			self::vd($_SESSION);
+			self::vd($_ENV);
+		}
+		
+	}
+}
+
+
+
+?>
