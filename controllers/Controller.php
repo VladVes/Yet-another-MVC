394f18b47a033c760ae060552a5ff16e99af008f
Vlad Ves <vccs@inbox.ru>
2017-05-14 22:56:04 +0300

    updated

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -5,25 +5,45 @@ use app\services\Log;
 abstract class Controller
 {
 	protected $defaultAction = 'index';
+	protected $action;
+	protected $defaultRenderer = 'dr';
+	protected $renderer;
 
-	public function __construct()
+	public function __construct($renderer = null)
 	{
-
+		$this->renderer = $renderer ?: $this->defaultRenderer;
+		Log::write(get_called_class() . " controller and renderer {$this->renderer}");
 	}
 
 	public function run($action = null)
 	{
 		echo "<br> controller running controller!";
-		if(!is_null($action)) {
-			$actionMethod = 'action' . ucfirst($action);
-		} else {
-			$actionMethod = 'action'. ucfirst($this->defaultAction);
-		}
-		Log::write('running {$actionMethod}');
-		$this->$actionMethod();
+		
+		$this->action = $action ?: $this->defaultAction;
+		$action = 'action' . ucfirst($this->action);
+		Log::write("running {$actionMethod}");
+
+		$this->beforeAction();
+		$this->$action();
+		$this->afterAction();
 	}
 
+	protected function beforeAction() 
+	{
+
+	}
+
+	protected function afterAction()
+	{
+
+	}
+
+	public function render($template, $params = [])
+	{
+		//$renderer = $this->renderer;
+		app\base\Application::call()->$this->renderer->run($template, $params);
+
+	}
 
 }
-
 ?>
