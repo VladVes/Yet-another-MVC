c57468c200f35a2ed15d6ab1c1772574d6e6dc0f
Vlad Ves <vccs@inbox.ru>
2017-05-30 15:17:00 +0300

    class User updated

--- models/User.php
+++ models/User.php
@@ -5,9 +5,35 @@ namespace app\models;
 class User
 {
 	protected $id;
-	protected $name;
-	protected $comment;
+	protected $login;
+	protected $password;
 	protected $email;
+	protected $sessionId;
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getCurrent()
+	{
+		session_start();
+		
+		$userId = $this->getUserId();
+		if($userId) {
+			return Application::call()->userrep->getById($userId);
+		}
+		return null;
+	}
+
+	public function getUserId()
+	{
+		$sid = Application::call()->auth->getSessionId();
+		if(!is_null($sid)){
+			return Application::call()->sessionrep->getUidBySid($sid);
+		}
+		return null;
+	}
 
 	
 }			
