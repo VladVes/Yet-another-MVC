5d87b877f42bfe4d656da433778081381eba92ea
Vlad Ves <vccs@inbox.ru>
2017-06-05 12:19:10 +0300

    auth service methods

--- services/Auth.php
+++ services/Auth.php
@@ -8,6 +8,20 @@ class Auth
 	public static $isLogged = false;
 	protected $sessionKey = 'sid';
 
+	public function login($login, $password, $rem)
+	{
+		$user = Application::call()->userrep->getByLoginPass($login, $password);
+		if (!$user) {
+			return false;
+		} elseif ($this->openSession($user)) {
+			self::$isLogged = true;
+		 	if ($rem) {
+		 		Application::call()->cookie->createCookie("token", $_SESSION[$this->sessionKey], time() + 3600 * 24 * 7 );
+		 	}
+		 	return true;
+		 } 
+	}
+
 	public function getSessionId() 
 	{
 		if (isset($_COOKIE['token'])) {
@@ -25,5 +39,23 @@ class Auth
 
 		} 
 		return $sid;
+	}
+	protected function openSession(User $user) {
+		$model = Application::call()->sessionrep;
+		$sid = $this->generateStr();
+		$model->createNew($user->getId(), $sid, date("Y-m-d H:i:s"));
+		$_SESSION[$this->sessionKey] = $sid;
+		return true;
+	}
+
+	private function generateStr($length = 10){
+		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789";
+        $code = "";
+        $clen = strlen($chars) - 1;
+
+        while (strlen($code) < $length)
+            $code .= $chars[mt_rand(0, $clen)];
+
+        return $code;
 	}
 }
