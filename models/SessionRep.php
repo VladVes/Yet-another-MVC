f219fc7ccb52e751bb75fc987f1d943bf40174d9
Vlad Ves <vccs@inbox.ru>
2017-06-06 11:49:42 +0300

    method

--- models/SessionRep.php
+++ models/SessionRep.php
@@ -19,5 +19,20 @@ class SessionRep {
 			"INSERT INTO sessions(user_id, sid, last_update) VALUES (?, ?, ?)", [$userId, $sid, $timeLast]
 		);
 	}
+	public function updateLastTime($sid, $time = null)
+    {
+        if (is_null($time)) {
+            $time = date('Y-m-d H:i:s');
+        }
+        return Application::call()->db->execute(
+            "UPDATE sessions SET last_update = '{$time}' WHERE sid = '{$sid}'");
+    }
+
+	public function getUidBySid($sid)
+	{
+		return Application::call()->db->fetchOne(
+			"SELECT user_id FROM sessions WHERE sid = ?", [$sid]
+		)['user_id'];
+	}
 
 }
